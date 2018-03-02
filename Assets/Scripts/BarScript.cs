184dba8756474040afcb97c451472fff643ea59b
Daniel Laggar <daniel.laggar@gmail.com>
2018-03-02 18:26:32 +0100

    HUD
    HUD med stamina och hälsa. Behöver poleras.

---
+++ Assets/Scripts/BarScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+    private float fillAmount;
+
+    [SerializeField]
+    private Image barImage;
+
+    [SerializeField]
+    private Text valueText;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    [SerializeField]
+    private Color lowColor;
+
+    [SerializeField]
+    private Color fullColor;
+
+    [SerializeField]
+    private bool lerpColor;
+
+    public float MaxValue { get; set; }
+
+    public float Value
+    {
+        set
+        {
+            string[] tmp = valueText.text.Split(':');
+            valueText.text = tmp[0] + ": " + value;
+            fillAmount = barCalc(value, 0, MaxValue, 0, 1);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		if (lerpColor)
+        {
+            barImage.color = fullColor;
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        HandleBar();
+	}
+
+    //Ser till att mätaren uppdateras om den inte redan är korrekt.
+    private void HandleBar()
+    {
+        if (fillAmount != barImage.fillAmount)
+        {
+            barImage.fillAmount = Mathf.Lerp(barImage.fillAmount, fillAmount, Time.deltaTime * lerpSpeed);
+        }
+
+        if (lerpColor)
+        {
+            barImage.color = Color.Lerp(lowColor, fullColor, fillAmount);
+        }
+        
+        
+    }
+
+    //Uträkning för att få currentHealth till ett värde mellan minBar(0) och maxBar(1)
+    private float barCalc(float currentHealth, float minHealth, float maxHealth, float minBar, float maxBar)
+    {
+        return (currentHealth - minHealth) * (maxBar - minBar) / (maxHealth - minHealth) + minBar;
+    }
+
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    [SerializeField]
+    private Stats health;
+
+    [SerializeField]
+    private Stats stamina;
+
+	// Use this for initialization
+	private void Awake ()
+    {
+        health.Initialize();
+        stamina.Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (stamina.CurrentValue < stamina.MaxValue)
+        {
+            stamina.CurrentValue += 1;
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            health.CurrentValue -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            health.CurrentValue += 10;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            stamina.CurrentValue -= 30;
+        }
+    }
+
+}
