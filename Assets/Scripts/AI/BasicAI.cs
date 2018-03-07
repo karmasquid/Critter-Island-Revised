358ed2f5ca2240b5215e6e3c3711c1c52ecafa99
karmasquid <felare9001@hotmail.com>
2018-03-07 14:56:59 +0100

    AI tweaking
    Jobbar på att få en roaming funktion igång.

--- Assets/Scripts/AI/BasicAI.cs
+++ Assets/Scripts/AI/BasicAI.cs
@@ -13,13 +13,15 @@ public class BasicAI : MonoBehaviour {
      * ATTACK
      * LOSE TARGET > IDLE > PATROLING
      * DEAD  */
-    bool playerInSight;
+   // bool playerInSight;
     int currentHP = 100;
     int maxHP = 100;
+    public int maxArmor = 0;
+    public int currentArmor = 0;
     public int attackDMG = 0;
-    public Transform targetPlayer;
-    public string playerTag = "Player";
-   
+    
+    Transform targetPlayer;
+    string playerTag = "Player";
     
     [SerializeField]
     float targetingRange = 10f;
@@ -28,71 +30,75 @@ public class BasicAI : MonoBehaviour {
     [SerializeField]
     float attackRange = 0f;
 
+    public float roamingRadius = 0f;
+    public float roamingTimer = 0f;
+    private Transform enemyAI;
+    private float timer;
+
     NavMeshAgent agent;
     
 	void Start ()
         {
-        playerInSight = false;
+      //  playerInSight = false;
         agent = GetComponent<NavMeshAgent>();
-      //  InvokeRepeating("Searching", 0f, 0.5f );
+        InvokeRepeating("Roaming", 0f, 0f);
+        InvokeRepeating("Searching", 0f, 0.5f );
         targetPlayer = PlayerManager.instance.player.transform;
         
 	    }
 
     void Searching()
         {
-        
+        Roaming();
 
-        
+        float distanceToPlayer = Vector3.Distance(targetPlayer.position, transform.position);
 
-         
-       /*GameObject[] playerMesh = GameObject.FindGameObjectsWithTag(playerTag);
-        float closestRange = Mathf.Infinity;
-        GameObject nearestPlayer = null; //Närmsta spelarmodellen som finns relativt till fienden med detta script.
-
-         foreach (GameObject player in playerMesh)
-            {
-            
-            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-            if(distanceToPlayer < closestRange) //Om räckvidden till spelaren är mindre än närmsta räckvidden betyder det att fienden hittat spelaren. 
-            {
-                
-                closestRange = distanceToPlayer;
-                nearestPlayer = player;
-            }
-            
-        }
-        if (nearestPlayer != null && closestRange <= targetingRange)
+        if (distanceToPlayer <= targetingRange && distanceToPlayer <= aggroRange)
         {
-            print("Hello!");
-            targetPlayer = nearestPlayer.transform;
-
             PlayerTargeted();
+            agent.SetDestination(targetPlayer.position);
+
+            if (distanceToPlayer <= attackRange)
+            {
+                //attack
+            }
         }
-        else
+        else if (distanceToPlayer > aggroRange)
         {
-            targetPlayer = null;
-        } */
+            return; // Gå tillbaka till att roama.
+        }
     }
 
     void PlayerTargeted()
     {
         print ("Hey you!");
-       // transform.position = new Vector3();
+        // Anim alarmed/approaching here
           
     }
-    void Update()
+
+    void Roaming()
     {
+        // Anim patrol here
+        
+    }
 
-        float distanceToPlayer = Vector3.Distance(targetPlayer.position, transform.position);
-
-        if (distanceToPlayer <= targetingRange)
+    public void TakeDMG()
+    {
+        /// Precis som metoden heter så ska skadan in här, 
+        /// tänk på att armor och HP ska vara aktiva variabler här.
+        if(currentHP <= 0)
         {
-            agent.SetDestination(targetPlayer.position);
+            Destroy(gameObject);
         }
     }
 
-    void OnDrawGizmosSelected () //Måste kunna se hur långt den kan göra olika saker.
+    public void DealDMG()
+    {
+        /// Damage ska skickas härifrån till spelaren, är det så att ett bättre sätt upptäcktes 
+        /// från spelarscriptet så kan denna tas bort.
+    }
+
+    void OnDrawGizmosSelected () //Ritar ut wirespheres när modellen väls i scenen. De Visar hur lång de olika rangen är.
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
