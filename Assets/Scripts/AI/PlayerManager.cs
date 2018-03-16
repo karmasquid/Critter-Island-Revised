7a23dc106378e9ef28bb28c9a44a4670c62d207f
Stinan <Stinan.H@gmail.com>
2018-03-16 12:33:46 +0100

    lagt in playermanager, fixat inventory och itemdatabase
    gettin shit done!

--- Assets/Scripts/AI/PlayerManager.cs
+++ Assets/Scripts/AI/PlayerManager.cs
@@ -4,15 +4,94 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
 
+    private Inventory inventoryScript;
+    private Movement movementScript;
+    private BarScript barScript;
+    private BasicAI basicAI;
+
+    private float health; // <------------ fråga daniel
+    private float armor;
+
+    private int ammoCount;
+
+    public GameObject player;
+
     #region Singleton
     public static PlayerManager instance;
     void Awake()
     {
-        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+
+        DontDestroyOnLoad(gameObject);
     }
     #endregion
-    
 
-    public GameObject player;
+    public void AddPlayerstats(Item itemAdd)
+    {
+        health += itemAdd.Health;
+        armor += itemAdd.Armor;
+
+        //Barscriptvariabel += itemAdd.StaminaRecovery;
+        ammoCount += itemAdd.AmmoCount;
+
+        //movmentscriptvariabel += itemAdd.MovementDiff;
+
+        //movementspeedstuff += itemAdd.AttackSpeed;
+    }
+
+    public void Removestats(Item itemrem)
+    {
+        health -= itemrem.Health;
+        armor -= itemrem.Armor;
+
+        //Datbarscriptfloat += itemAdd.StaminaRecovery
+
+        ammoCount = itemrem.AmmoCount;
+        //movementscript -= itemrem.MovementDiff;
+        //movementscriptstuff += itemrem.AttackSpeed;
+    }
+
+    public void MeleeAttack()
+    {
+        //barscript -= stamina för rätt item.
+
+        
+
+
+    }
+
+    public void RangeAttack(GameObject enemy)
+    {
+        if (ammoCount > 0)
+      {
+            ammoCount -= 1;
+            //barscript -stamina
+            basicAI = enemy.GetComponent<BasicAI>();
+            //aiScript.currentHP  <----------fixa en set.
+       }
+    }
+
+    public void SpecAttack(GameObject enemy)
+    {
+        //Remove stamina
+        //Deal damage.
+    }
+
+    public void TakeDamage(int Damage)
+    {
+        //float damagedealt = Damage
+    }
+
+
+
 
 }
--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -31,32 +31,30 @@ public class ItemDatabase : MonoBehaviour
     private void Start()
     {
 
-        //(int Id, string Name, string Desc, TypeOfItem KindOfGear, int DamageBuff, int HealthBuff, float StaminaRechargeBuff, int Damage, bool InfiniteAmmo, int AmmoCount, float FireRate, float MeleeRange, float ThrowRange, int StaminaLoss)
+        //()
 
         //Weapons
-        allItems.Add(new Item(1, "W1", "Weapon 1 is Supermegaawsome", Item.TypeOfItem.Weapon, 5, 0, 0f, 10, true, 0, 0f, 0.5f, 0f, 7));
-        allItems.Add(new Item(2, "W2", "Supermegaawsome weapon2!", Item.TypeOfItem.Weapon, 10, 0, 0f, 15, true, 0, 0f, 0.7f, 0f, 10));
+        allItems.Add(new Item(1, "W1", "Weapon 1 is Supermegaawsome", Item.TypeOfItem.Weapon, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100 ,0f, 0f, 1,5));
+        allItems.Add(new Item(2, "W2", "Supermegaawsome weapon2!", Item.TypeOfItem.Weapon, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
 
         //Equippable
-        allItems.Add(new Item(3, "e1", "Ba-Boom!", Item.TypeOfItem.Equippable, 0, 0, 0f, 20, false, 15, 2f, 0f, 5f, 3));
-        allItems.Add(new Item(4, "e2", "Ba-Boom!", Item.TypeOfItem.Equippable, 0, 0, 0f, 25, false, 10, 2f, 0f, 5f, 3));
+        allItems.Add(new Item(3, "e1", "Ba-Boom!", Item.TypeOfItem.Ranged, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
+        allItems.Add(new Item(4, "e2", "Ba-Boom!", Item.TypeOfItem.Ranged, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
 
         //Helmets
-        allItems.Add(new Item(5, "h1", "Helmet1 is helmet1!", Item.TypeOfItem.Helmet, 0, 5, 0.5f, 0, false, 0, 0, 0, 0, 0));
-        allItems.Add(new Item(6, "h2", "Helmet1 is helmet1!", Item.TypeOfItem.Helmet, 0, 3, 0.5f, 0, false, 0, 0, 0, 0, 0));
-
+        allItems.Add(new Item(5, "h1", "Helmet1 is helmet1!", Item.TypeOfItem.Headgear, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
+        allItems.Add(new Item(6, "h2", "Helmet1 is helmet1!", Item.TypeOfItem.Headgear, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
         //Armors
-        allItems.Add(new Item(7, "a1", "armor de la awsome!", Item.TypeOfItem.Armor, 0, 5, 0.5f, 0, false, 0, 0, 0, 0, 0));
-        allItems.Add(new Item(8, "a2", "another awsome armor.", Item.TypeOfItem.Armor, 0, 5, 1f, 0, false, 0, 0, 0, 0, 0));
+        allItems.Add(new Item(7, "a1", "armor de la awsome!", Item.TypeOfItem.Armor, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
+        allItems.Add(new Item(8, "a2", "another awsome armor.", Item.TypeOfItem.Armor, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
 
         //Boots
-        allItems.Add(new Item(9, "b1", "Fart away!", Item.TypeOfItem.Boots, 0, 5, 1f, 0, false, 0, 0, 0, 0, 0));
-        allItems.Add(new Item(10, "b2", "Super pretty boots!", Item.TypeOfItem.Boots, 0, 5, 1.5f, 0, false, 0, 0, 0, 0, 0));
+        allItems.Add(new Item(9, "b1", "Fart away!", Item.TypeOfItem.Boots, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
+        allItems.Add(new Item(10, "b2", "Super pretty boots!", Item.TypeOfItem.Boots, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
 
         //Miscellaneous
-        allItems.Add(new Item(9, "m1", "unlocks secret door", Item.TypeOfItem.Miscellaneous, 0, 0, 0f, 0, false, 0, 0f, 0f, 0f, 0));
-        allItems.Add(new Item(10, "m2", "unlocks a supersecret door", Item.TypeOfItem.Miscellaneous, 0, 0, 0f, 0, false, 0, 0f, 0f, 0f, 0));
-
+        allItems.Add(new Item(9, "m1", "unlocks secret door", Item.TypeOfItem.Miscellaneous, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
+        allItems.Add(new Item(10, "m2", "unlocks a supersecret door", Item.TypeOfItem.Miscellaneous, 10f, 0f, 5f, 0f, 0f, 5f, 15f, 0f, 0f, 100, 0f, 0f, 1, 5));
         inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
 
         if (inventory != null)
