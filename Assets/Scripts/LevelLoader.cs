c0a6f3cf83773d9fdf244172d036d5b4d2e31b3d
Daniel Laggar <daniel.laggar@gmail.com>
2018-03-20 16:07:54 +0100

    Loading Screen
    Gjort en level loader och att "new game" laddar prototypscenen med en progress bar. Ljuset blir skumt efter detta men det fungerar normalt om man gör en build och spelar. Behöver fixas senare.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void Loadlevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynch(sceneIndex));
+    }
+
+    IEnumerator LoadAsynch (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            //Gör om Unitys laddningsprocent till ett tal mellan 0 - 1 istället för Unitys 0 - 0.9
+            float progress = Mathf.Clamp01(operation.progress) / 0.9f;
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        
     }
 
     public void QuitGame()
