7c05e20906eba7a94b3ae3098be7d4717ee0b984
karmasquid <felare9001@hotmail.com>
2018-03-26 16:09:31 +0200

    animation
    lagt in ett par parametrar nu så att åtminståne en animation spelas när man går fram med W. Vi kommer att behöva använda oss av bools och liknande.

--- Assets/Scripts/TempAnim.cs
+++ Assets/Scripts/TempAnim.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TempAnim : MonoBehaviour {
+    bool moving, idle, dodge, dodgeB, throwing, death;
 
-	// Use this for initialization
+    public Animator anim;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("BOOP");
+            anim.SetBool("isRunning", true);
+        }
+       if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+	
 	void Start () {
-		
-	}
+        anim = GetComponent<Animator>();
+        moving = false;
+
+        
+    }
+
 
 }
+
