da35375c95c129740626a58f4ae07373d0c31612
Stinan <Stinan.H@gmail.com>
2018-04-26 12:53:33 +0200

    throwable script och lootbag collider borttagen

---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Throwable : MonoBehaviour {
+
+
+    int damage;
+
+    Rigidbody rb;
+
+    public int Damage
+    {
+        set
+        {
+            damage = value;
+        }
+    }
+
+    void Start()
+    {
+        rb = this.transform.GetComponent<Rigidbody>();
+
+        rb.AddForce(this.transform.forward * 20, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyStats>().TakeDamange(damage);
+
+        }
+        Destroy(gameObject); // Destroy if collison.
+
+    }
+}
