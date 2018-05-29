1127df4c80f0157193cf5423db01795da58ab1c5
Afro663 <afro663@hotmail.com>
2018-05-29 14:40:24 +0100

    Wind particles + Part logic fix
    * New wind-like particles in docks
    * Added particle materials/sprites.

--- Assets/Scripts/WindScript.cs
+++ Assets/Scripts/WindScript.cs
@@ -6,61 +6,28 @@ using UnityEngine.AI;
 public class WindScript : MonoBehaviour
 {
     [SerializeField]
-    float offsetTimer;
-
-    bool emitting = true;
-
-    [SerializeField]
     ParticleSystem WindPartical;
 
     GameObject player;
     void Start()
     {
-        InvokeRepeating("OnOffEmission", 0f, offsetTimer);
         player = GameObject.Find("Player");
     }
-    //TODO When time for actual particalls is set -> Add Coroutine
-    void OnOffEmission()
-    {
-        if (!emitting)
-        {
 
-            WindPartical.Play();
-            emitting = true;
-        }
-        else
-        {
-            WindPartical.Stop();
-            emitting = false;
-        }
-    }
     void OnTriggerStay(Collider other)
     {
-        //SOLUTION A
-        if (other.gameObject.name == "Player" && emitting)
-        {
-            player.GetComponent<NavMeshAgent>().enabled = false;
-            player.transform.position = Vector3.MoveTowards(player.transform.position, WindPartical.transform.forward, 10 * Time.fixedDeltaTime);
-            print("You are now falling now...");
-            //player.GetComponent<NavMeshAgent>().enabled = true;
-        }
-        else if (!emitting)
-        {
-                print("Not falling down...");
-        }
-        /* SOLUTION B
         if (other.gameObject.name == "Player")
         {
             if (!player.GetComponent<Character>().dodging)
             {
                 player.GetComponent<NavMeshAgent>().enabled = false;
-                player.transform.position = Vector3.MoveTowards(player.transform.position, WindPartical.transform.forward, 10 * Time.deltaTime);
+                player.transform.position = Vector3.MoveTowards(player.transform.position, WindPartical.transform.forward, 5 * Time.deltaTime);
             }
             else
             {
                 print("You got passed.");
             }
         }
-        */
+        
     }
 }
